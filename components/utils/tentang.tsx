b0caec5255a6c7c042aafd67a8010398c920020a
Yuta4u <yudhistira4u@gmail.com>
2024-03-03 23:14:51 +0700

    update:solve err

--- components/utils/tentang.tsx
+++ components/utils/tentang.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Tentang = () => {
   return (
-    <div id="tentang" className="w-full py-0 pb-20 border-t-2 mt-20 md:py-10 ">
+    <div id="tentang" className="w-full py-0 pb-20 border-t-2 mt-20 md:py-10">
       <Image
         src={"/images/logo-text.png"}
         width={300}
@@ -16,7 +16,7 @@ const Tentang = () => {
         <span className="text-[#FA6709]"> D'Ganteng Solution</span> agar semua
         <br />
         lapisan masyarakat dapat merasakat kemudahan dalam mencukur
-        <br /> rambut dimanapun dan kapanppun.{" "}
+        <br /> rambut dimanapun dan kapanpun.
         <span className="text-[#FA6709]">Mister Cukur</span> hadir untuk menjadi
         <br />
         solusi bagi kalian yang memiliki waktu terbatas dan mageran. Klik
